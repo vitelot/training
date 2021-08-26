57e5baa7b50e6ef129427d8d7696b8f87cc9db39
Vito DP Servedio <v.servedio@gmail.com>
2021-08-26 18:03:34 +0200

    script to look into single block delays

--- analysis/alltransits.jl
+++ analysis/alltransits.jl
@@ -11,6 +11,7 @@ returns the number of seconds elapsed from the epoch #midnight
 end
 
 struct Transit
+    bs::String
     duetime::Int
     realtime::Int
 end
@@ -20,10 +21,14 @@ function main()
 
     df = CSV.File("data.csv") |> DataFrame
     dropmissing!(df, [:duetime, :realtime])
+    # allowmissing!(df)
+    # df .= ifelse.(ismissing.(df), "XXXXXX", df)
 
     dayList = unique(df.day)
 
     for day in dayList
+        outfile = "$day.dat"
+        println(day)
 
         Travel = Dict{String,Vector{Transit}}()
 
@@ -33,34 +38,41 @@ function main()
 
         for i = 1:nrow(dfday)
             treno = dfday.treno[i]
+            # println(dfday[i, :])
+            # if ismissing(dfday.bscode)
+            # end
+            bs = coalesce(dfday.bscode[i],"XXXXXX") #ismissing(dfday.bscode) ? "XXXXXX" : dfday.bscode
             rt = dateToSeconds(dfday.realtime[i])
             dt = dateToSeconds(dfday.duetime[i])
             get!(Travel, treno, Transit[])
-            push!(Travel[treno], Transit(dt,rt))
+            push!(Travel[treno], Transit(bs,dt,rt))
         end
 
+        OUT = open(outfile, "w")
         for treno in keys(Travel)
             journey = Travel[treno]
             l = length(journey)
             if l < 2 continue end
 
             for i in 1:l-1
+                blk = journey[i].bs * "," * journey[i+1].bs
                 dt = journey[i+1].duetime - journey[i].duetime
                 rt = journey[i+1].realtime - journey[i].realtime
-                dt > 0 && push!(Delays[day], rt/dt)
+                println(OUT, "$day,$treno,$blk,$dt,$rt,$(round(rt/dt, digits=3))")
             end
 
         end
-    end
-
-    for day in keys(Delays)
-        outfile = "$day.dat"
-        sort!(Delays[day], rev=true)
-        data = DataFrame(delay=Delays[day])
-        CSV.write(outfile, data, writeheader=false)
+        close(OUT)
     end
 
     nothing
 end
 
 main()
+
+# Problems
+# 04.02.19,R2212,HET,Abfahrt,04.02.2019 08:09:30,04.02.2019 08:14:04
+# 04.02.19,R2212,HET,Ankunft,04.02.2019 08:09:30,04.02.2019 08:13:42
+#
+# 04.02.19,R22342,BU,Ankunft,04.02.2019 08:12:54,04.02.2019 08:14:03
+# 04.02.19,R22342,BU,Abfahrt,04.02.2019 08:13:24,04.02.2019 08:14:04
