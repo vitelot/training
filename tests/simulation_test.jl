364cdcd40d7ead6cd885b20d8ced8230a54bc5e8
McDaniots <daniottisimone@gmail.com>
2022-01-11 18:37:09 +0100

    modif to run test on main

--- tests/simulation_test.jl
+++ tests/simulation_test.jl
@@ -2,13 +2,16 @@ using Test
 
 @testset "Simulation" begin
 
-    cmd=`julia --project=training_env ./simulation/main.jl ./tests/data/par_test.ini`
+
     original_stdout=stdout
     (out,_)=redirect_stdout()
 
+    cmd=`julia --project=training_env ./simulation/main.jl ./tests/data/par_test.ini`
     run(cmd)
+
     new_stdout=readline(out)
     redirect_stdout(original_stdout);
+    
     str="Total delay at the end of simulation is 42"
 
     @test new_stdout==str
