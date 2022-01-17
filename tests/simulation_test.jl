4ae274dff96345c0deb75d8c4abf283aff1e3f75
McDaniots <daniottisimone@gmail.com>
2022-01-17 14:21:09 +0100

    maybe found

--- tests/simulation_test.jl
+++ tests/simulation_test.jl
@@ -6,12 +6,12 @@ using Test
     original_stdout=stdout
     (out,_)=redirect_stdout()
 
-    cmd=`julia --project=training_env ./simulation/main.jl ./tests/data/par_test.ini`
+    cmd=`julia --project=training_env --code-coverage ./simulation/main.jl ./tests/data/par_test.ini`
     run(cmd)
 
     new_stdout=readline(out)
     redirect_stdout(original_stdout);
-    
+
     str="Total delay at the end of simulation is 42"
 
     @test new_stdout==str
