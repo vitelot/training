de9cd2e1b17ff7189d6999e3b9a92130fc46fc9d
McDaniots <daniottisimone@gmail.com>
2022-01-17 17:12:59 +0100

    better test in github actions

--- simulation/main.jl
+++ simulation/main.jl
@@ -7,6 +7,35 @@ include("simulation.jl")
 using Profile
 using InteractiveUtils
 
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--opt1"
+            help = "an option with an argument"
+        "--opt2", "-o"
+            help = "another option with an argument"
+            arg_type = Int
+            default = 0
+        "--flag1"
+            help = "an option without argument, i.e. a flag"
+            action = :store_true
+
+        "--ini", "-i"
+            help = "path for the .ini file"
+            arg_type = String
+            default = "../data/simulation_data/par.ini"
+        # "arg1"
+        #     help = "a positional argument"
+        #     required = true
+    end
+
+    return parse_args(s)
+end
+
+
 function main()
 
     if VERSION < v"1.6"
@@ -14,13 +43,21 @@ function main()
         exit()
     end
 
+    parsed_args = parse_commandline()
+
+
+    file=parsed_args["ini"]
+
+    loadOptions(file);
+
+
     #if passed an argument, it is the input file path
-    if isempty(ARGS)
-        file = "../data/simulation_data/par.ini"
-        loadOptions(file);
-    else
-        loadOptions(ARGS[1])
-    end
+    # if isempty(ARGS)
+    #     file = "../data/simulation_data/par.ini"
+    #     loadOptions(file);
+    # else
+    #     loadOptions(ARGS[1])
+    # end
 
     Opt["print_flow"] && println("Options loaded, starting the program.")
 
@@ -49,7 +86,7 @@ function main()
         else
             return (RN,FL)
         end
-        
+
     end
     nothing
 end
