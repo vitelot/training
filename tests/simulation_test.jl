244c39ab8161eee42d57207483df7f7e096c40d3
McDaniots <daniottisimone@gmail.com>
2022-01-17 17:30:53 +0100

    new better test using argparse

--- tests/simulation_test.jl
+++ tests/simulation_test.jl
@@ -2,17 +2,22 @@ using Test
 
 @testset "Simulation" begin
 
+    fname="./run/out"
+    out=read(fname, String)
 
-    original_stdout=stdout
-    (out,_)=redirect_stdout()
-
-    cmd=`julia --project=training_env --code-coverage ./simulation/main.jl ./tests/data/par_test.ini`
-    run(cmd)
-
-    new_stdout=readline(out)
-    redirect_stdout(original_stdout);
+    # original_stdout=stdout
+    # (out,_)=redirect_stdout()
+    #
+    # cmd=`julia --project=training_env --code-coverage ./simulation/main.jl --ini ./tests/data/par_test.ini ">" ./run/out` #
+    # run(cmd)
+    #
+    # new_stdout=readline(out)
+    # redirect_stdout(original_stdout);
 
     str="Total delay at the end of simulation is 42"
+
+    println(out)
+    println(str)
 
     @test new_stdout==str
 end
