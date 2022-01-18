6695b6c9f0328f61e9f5317b77f8ae9107877136
McDaniots <daniottisimone@gmail.com>
2022-01-18 14:02:07 +0100

    added parser

--- simulation/main.jl
+++ simulation/main.jl
@@ -3,37 +3,10 @@ include("initialize.jl")
 include("parameters.jl")
 include("functions.jl")
 include("simulation.jl")
+include("parser.jl")
 
 using Profile
 using InteractiveUtils
-
-using ArgParse
-
-function parse_commandline()
-    s = ArgParseSettings()
-
-    @add_arg_table s begin
-        "--opt1"
-            help = "an option with an argument"
-        "--opt2", "-o"
-            help = "another option with an argument"
-            arg_type = Int
-            default = 0
-        "--flag1"
-            help = "an option without argument, i.e. a flag"
-            action = :store_true
-
-        "--ini", "-i"
-            help = "path for the .ini file"
-            arg_type = String
-            default = "../data/simulation_data/par.ini"
-        # "arg1"
-        #     help = "a positional argument"
-        #     required = true
-    end
-
-    return parse_args(s)
-end
 
 
 function main()
---
+++ simulation/parser.jl
@@ -0,0 +1,27 @@
+using ArgParse
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--opt1"
+            help = "an option with an argument"
+        "--opt2", "-o"
+            help = "another option with an argument"
+            arg_type = Int
+            default = 0
+        "--flag1"
+            help = "an option without argument, i.e. a flag"
+            action = :store_true
+
+        "--ini", "-i"
+            help = "path for the .ini file"
+            arg_type = String
+            default = "../data/simulation_data/par.ini"
+        # "arg1"
+        #     help = "a positional argument"
+        #     required = true
+    end
+
+    return parse_args(s)
+end
