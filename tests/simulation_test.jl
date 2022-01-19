c2a86f442fa2863439b3e39f985ee474dce11f22
McDaniots <daniottisimone@gmail.com>
2022-01-19 11:06:19 +0100

    test

--- tests/simulation_test.jl
+++ tests/simulation_test.jl
@@ -25,6 +25,7 @@ include("../simulation/functions.jl")
     BK=Dict{String,Block}("FLDH1-FLDU2" => Block("FLDH1-FLDU2", 1, 0, Set{String}()), "FLDU2-FLDH1" => Block("FLDU2-FLDH1", 1, 0, Set{String}()))
     hashing=true
 
-
     @test netStatus(S, BK; hashing)==0xbd32f78d463d7cfb
+    @test netStatus(S, BK; false)==0xbd32f78d463d7cfb
+    @test netStatus(Set{String}(), BK; hashing)==""
 end
