5437642b046b2c246cde9e507f573ed0d393ecdf
Nickolson88 <cinardi@csh.ac.at>
2022-03-25 14:56:08 +0100

    Modified function to assign exo. delays

--- preprocessing/exo_delays.jl
+++ preprocessing/exo_delays.jl
@@ -5,7 +5,8 @@ function pwl()
 """
 It extracts a random value from the power law distribution
 """
-    return 60*rand(Float64)^(1/(1-2.16))   #60s is the mimimum delay value in the power-law distribution , 2.16 is the exponent
+    d=60*rand(Float64)^(1/(1-2.5))   #60s is the mimimum delay value in the power-law distribution , 2.16 is the exponent
+    return round(Int64, d)
 end
 
 
@@ -15,32 +16,23 @@ It takes the trains from the timetable, assign to them a probability based on a 
 extracts the number n of exo. delays that has to be inserted, extract the BSTs,
 build the csv with exo. delays to be assigned
 """
-    dft=DataFrame(CSV.File("tipica.csv"))           #Or Mon, Tue, ... Or timetable
+    dft=DataFrame(CSV.File("tipicaok.csv"))           #Or Mon, Tue, ... Or timetable
     dtrains=unique(dft.trainid)
 
     dfp=DataFrame(CSV.File("probabilities.csv"))
     dfp=filter(:trainid => x-> x ∈dtrains,dfp)
 
 
-    d=LogNormal(4.26,0.46)  #Total number of exogeneous delays we inject
-    n=round(Int,rand(d))
+    d=LogNormal(4.26,0.46)
+    n=round(Int,rand(d))              #Total number of exogeneous delays we inject
+
+    sample_rows = sample(1:nrow(dfp), StatsBase.Weights(dfp.prob), n)  #It samples n rows based on their probabilities
+    dfp = dfp[sample_rows, :]
+
+    dfp[!,:delay] = [pwl() for i in 1:n]        # This adds the column of n random delays
+
+    dfp=select!(dfp, Not(:prob))
 
 
-    dtrains=Vector{String}()
-    for i in 1:n
-        t=sample(dfp.trainid, StatsBase.Weights(dfp.prob))
-        push!(dtrains,t)
-    end
-
-    BT=Dict{String,String}()
-    for t in dtrains
-        df=filter(:trainid => ==(t),dft)
-        BT[t]=rand(df.opid)
-    end
-
-    bst=[BT[i] for i in dtrains]
-    delay=[pwl() for i in 1:n]
-    df=DataFrame([dtrains,bst,delay], [:trainid, :opid,:delay])
-
-    CSV.write("exo_delays", df, header=false)
+    CSV.write("exo_delays", dfp, header=false)
 end
