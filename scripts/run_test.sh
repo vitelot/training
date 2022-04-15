872bb3ea9d76311bd519b9d2794b3a847e4763d8
vitelot <v.servedio@gmail.com>
2022-04-15 10:19:45 +0200

    removed toy_data, using tests/data again

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -1,3 +1,3 @@
 echo "Running a test using a proxy timetable and a fake railway structure"
 cd ../
-julia --project=training_env ./simulation/main.jl --ini ./data/toy_data/par_test_default.ini
+julia --project=training_env ./simulation/main.jl --ini ./tests/data/par_test_default.ini
