39a8d08b5a13dfd9da99a9d064a55a98155f6729
vitelot <v.servedio@gmail.com>
2022-05-12 12:40:05 +0200

    changed all scripts accordingly

--- scripts/run_multi_exo_delays.sh
+++ scripts/run_multi_exo_delays.sh
@@ -4,9 +4,9 @@ echo "has to contain the name of the folder with the delay files."
 echo "The default day 25.03.19 is used."
 
 cd ../preprocessing
-echo "julia --project=../training_env preprocessing.jl --exo_delays 90"
-julia --project=../training_env preprocessing.jl --exo_delays 90
+echo "julia --project=../environment preprocessing.jl --exo_delays 90"
+julia --project=../environment preprocessing.jl --exo_delays 90
 
 cd ../simulation
-echo "julia --project=../training_env main.jl --inject_delays --multi_simulation"
-julia --project=../training_env main.jl --inject_delays --multi_simulation
+echo "julia --project=../environment main.jl --inject_delays --multi_simulation"
+julia --project=../environment main.jl --inject_delays --multi_simulation
--- scripts/run_trycatch.sh
+++ scripts/run_trycatch.sh
@@ -1,5 +1,5 @@
 echo "The simulation will try to infer the infrastructure based on the trains circulating."
 cd ../simulation
 
-echo "julia --project=../training_env main.jl --catch_conflict_flag"
-julia --project=../training_env main.jl --catch_conflict_flag
+echo "julia --project=../environment main.jl --catch_conflict_flag"
+julia --project=../environment main.jl --catch_conflict_flag
