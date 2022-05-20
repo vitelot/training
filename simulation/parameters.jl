6a75ecbf067b534f6339b681aa97dbc2dd889ea2
vitelot <v.servedio@gmail.com>
2022-05-20 15:25:56 +0200

    removed opid from par.ini; corrected speed test in codcov

--- simulation/parameters.jl
+++ simulation/parameters.jl
@@ -112,24 +112,23 @@ Version                 $ProgramVersion # Program's version
 #############################
 timetable_file          ../data/simulation_data/timetable.csv # contains the timetable to simulate
 block_file              ../data/simulation_data/blocks.csv    # contains the nr of tracks for each block
-opoint_file             Unused # it would contain info on the operational points
 trains_info_file        None   # contains info on all the trains (direction, line, etc.)
 imposed_delay_repo_path None   # contains files with delay assignments, e.g., ../data/delays/
 #############################
 simulate                1   # if false do not run the simulation but load the data and exit RN,FL
 #############################
-print_options           1   # print out these options
-print_flow              1   # notify when a new function starts and ends
+print_options           0   # print out these options
+print_flow              0   # notify when a new function starts and ends
 print_train_status      0   # notify the status of trains on operation points
 print_new_train         0   # notify when a new train is found in the timetable
 print_train_wait        0   # notify when a train has to wait because next block is occupied
-print_train_end         1   # display train status at their final destination
+print_train_end         0   # display train status at their final destination
 print_train_fossile     0   # display trains that appear in one station only
 print_train_list        0   # display the id of processed trains
 print_elapsed_time      0   # display elapsed simulated seconds
-print_imposed_delay     1   # display trains with imposed delay
+print_imposed_delay     0   # display trains with imposed delay
 print_tot_delay         1   # print the total delay at the end of simulation
-print_notifications     1   # the simulation sequential number in stderr
+print_notifications     0   # the simulation sequential number in stderr
 print_timetable         0   # print a timetable similar to input in file
 #############################
 
