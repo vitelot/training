d16862a270f59032951a238fa4cb0dbfb93031fc
vitelot <v.servedio@gmail.com>
2022-12-14 13:22:42 +0100

    get ops and vehicles from railml

--- preprocessing/RailML2CSV.jl
+++ preprocessing/RailML2CSV.jl
@@ -1,43 +1,77 @@
 using CSV, DataFrames, LightXML;
 
 cd("preprocessing");
+infile = "data/railml_2022-08-05.xml";
 
 UString = Union{String,Missing};
 
-xdoc = parse_file("data/railml_2022-08-05.xml");
+function getInfra(xroot::XMLElement)::DataFrame
+    ##### INFRASTRUCTURE SCHEMA #####
+    # <infrastructure id="IS_01" name="infrastructure" timetableRef="TT_01" rollingstockRef="RS_01">
+    #   <operationControlPoints>
+    #     <ocp id="ocp_MV_2021-12-11_230000" code="3149" name="MV" description="Mitterdorf-Veitsch" parentOcpRef="ocp_MV_2021-12-11_230000">
+    #       <propOperational operationalType="station"/>
+    #       <geoCoord coord="47.536702 15.513335"/>
+    #       <designator register="DB640" entry="Mv" startDate="2021-12-11" endDate="2022-12-10"/>
+    #       <designator register="PLC" entry="3149" startDate="2021-12-11" endDate="2022-12-10"/>
+    #     </ocp>
+    #   </operationControlPoints>
+    # </infrastructure>
+    Infra = xroot["infrastructure"];
+    Ops = Infra[1]["operationControlPoints"][1]["ocp"];
+
+    df = DataFrame(id=UString[], name=UString[], description=UString[], coordinates=UString[]);
+
+    for o in Ops
+        id = attribute(o, "id");
+        name = attribute(o, "name");
+        descr = attribute(o, "description");
+        # println(id);
+        coord = length(o["geoCoord"]) == 0 ? missing : attribute(o["geoCoord"][1], "coord"); 
+        
+        push!(df, (id,name,descr,coord));
+    end
+    return df;
+end
+
+function getVehicles(xroot::XMLElement)::DataFrame
+    vehicles = xroot["rollingstock"][1]["vehicles"][1]["vehicle"];
+
+    df = DataFrame(id=UString[], code=UString[], description=UString[], name=UString[], category=UString[]);
+    for v in vehicles
+        # id::UString=missing;
+        code::UString=missing;
+        descr::UString=missing;
+        name::UString=missing;
+        cat::UString=missing;
+
+        
+        D = attributes_dict(v);
+        code  = get(D, "code", missing);
+        descr = get(D, "description", missing);
+        name  = get(D, "name", missing);
+        cat   = get(D, "vehicleCategory", missing);
+
+        # println(D["id"]);
+        push!(df, (D["id"], code, descr, name, cat));
+
+    end
+    return df;
+end
+
+xdoc = parse_file(infile);
 
 # get the root element
 xroot = root(xdoc);  # an instance of XMLElement: <railml>
 # print its name
 println(name(xroot))  # this should print: bookstore
 
-##### INFRASTRUCTURE SCHEMA #####
-# <infrastructure id="IS_01" name="infrastructure" timetableRef="TT_01" rollingstockRef="RS_01">
-#   <operationControlPoints>
-#     <ocp id="ocp_MV_2021-12-11_230000" code="3149" name="MV" description="Mitterdorf-Veitsch" parentOcpRef="ocp_MV_2021-12-11_230000">
-#       <propOperational operationalType="station"/>
-#       <geoCoord coord="47.536702 15.513335"/>
-#       <designator register="DB640" entry="Mv" startDate="2021-12-11" endDate="2022-12-10"/>
-#       <designator register="PLC" entry="3149" startDate="2021-12-11" endDate="2022-12-10"/>
-#     </ocp>
-#   </operationControlPoints>
-# </infrastructure>
-Infra = xroot["infrastructure"];
-Ops = Infra[1]["operationControlPoints"][1]["ocp"];
+dfinfra = getInfra(xroot);
+# CSV.write("data/infra.csv", dfinfra);
 
-df = DataFrame(id=UString[], name=UString[], description=UString[], coordinates=UString[]);
+dfvehicles = getVehicles(xroot);
+# CSV.write("data/vehicles.csv", dfvehicles);
 
-for o in Ops
-    id = attribute(o, "id");
-    name = attribute(o, "name");
-    descr = attribute(o, "description");
-    # println(id);
-    coord = length(o["geoCoord"]) == 0 ? missing : attribute(o["geoCoord"][1], "coord"); 
-    
-    push!(df, (id,name,descr,coord));
-end
-
-CSV.write("data/infra.csv", df);
 
 # traverse all its child nodes and print element names
 for c in child_nodes(xroot)  # c is an instance of XMLNode
