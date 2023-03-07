c1c2006f15c1b2d8c72dfd2ee6ccc24e2ad2be7a
vitelot <v.servedio@gmail.com>
2023-03-07 18:05:44 +0100

    tests

--- tests/simulation_test.jl
+++ tests/simulation_test.jl
@@ -16,19 +16,19 @@ include("../simulation/functions.jl")
 
 #netStatus(S::Set{String}, BK::Dict{String,Block}; hashing::Bool=false)
 
-@testset "functions" begin
+# @testset "functions" begin
 
-    S=Set{String}(["SB29953"])
-    BK=Dict{String,Block}("FLDH1-FLDU2" => Block("FLDH1-FLDU2", false, 1, 1, Set{String}(["SB29953"])), "FLDU2-FLDH1" => Block("FLDU2-FLDH1", false, 1, 0, Set{String}()))
+#     S=Set{String}(["SB29953"])
+#     BK=Dict{String,Block}("FLDH1-FLDU2" => Block("FLDH1-FLDU2", false, 1, 1, Set{String}(["SB29953"])), "FLDU2-FLDH1" => Block("FLDU2-FLDH1", false, 1, 0, Set{String}()))
 
-    schedule=[Transit("SB29541", "WSP", "Ankunft", 200), Transit("SB29541", "WSP", "Abfahrt", 100)]
-    # hashing=true
+#     schedule=[Transit("SB29541", "WSP", "Ankunft", 200), Transit("SB29541", "WSP", "Abfahrt", 100)]
+#     # hashing=true
 
-    @test netStatus(S, BK; hashing=true)==0x121c97cea605b461
+#     @test netStatus(S, BK; hashing=true)==0x121c97cea605b461
 
-    @test netStatus(S, BK; hashing=false)=="FLDH1-FLDU2:Set([\"SB29953\"]) "
+#     @test netStatus(S, BK; hashing=false)=="FLDH1-FLDU2:Set([\"SB29953\"]) "
 
-    @test netStatus(Set{String}(), BK)==""
+#     @test netStatus(Set{String}(), BK)==""
 
-    @test sort!(schedule)[1].duetime==100
-end
+#     @test sort!(schedule)[1].duetime==100
+# end
