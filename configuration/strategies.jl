c1ce4af302f86f4e782523a63062c9fee724b84d
Snehal M. Shekatkar <snehal@inferred.in>
2023-06-20 13:52:56 +0200

    Fix the bug related to rerouting

--- configuration/strategies.jl
+++ configuration/strategies.jl
@@ -92,6 +92,7 @@ end
 
 function reroute_sudbahn_to_pottendorfer!(df_full; trainid="RJ_130")
 
+
     # Extract sub-dataframe corresponding to the given trainid
     df = filter(row -> row[:trainid] == trainid, df_full)
 
@@ -103,46 +104,82 @@ function reroute_sudbahn_to_pottendorfer!(df_full; trainid="RJ_130")
     #=
      If the train stops at NB and MI, there are two rows corresponding to each of these
      One row for arrival, another for departure. Pick the second (departure) row of NB
-     and arrival row of MI, and change the rows between these two
+     and arrival row of MI, and change the rows between these two. If the train starts 
+     at NB, there would be only one corresponding row, choose the only element
     =#
-    row_start = NB_rows[2] + 1
+    if length(NB_rows) == 2
+        row_start = NB_rows[2] + 1
+        df_upto_NB = df[1:NB_rows[1], :]
+    else
+        row_start = NB_rows[1] + 1
+        df_upto_NB = DataFrame()
+    end
     row_end = MI_rows[1]-1
 
-    df_upto_NB = df[1:NB_rows[1], :]
-    df_after_MI = df[MI_rows[2]:end, :]
+    if length(MI_rows) == 2
+        df_after_MI = df[MI_rows[2]:end, :]
+    else
+        #df_after_MI = df[MI_rows[1]:end, :]
+        df_after_MI = DataFrame()
+    end
     # We need block_length column in df_after_MI to adjust distance column
     # after rerouting
-    df_after_MI[!, :block_length] .= df_after_MI[!, :distance] .- df_after_MI[1, :distance]
+    if nrow(df_after_MI) > 0
+        df_after_MI[!, :block_length] .= df_after_MI[!, :distance] .- df_after_MI[1, :distance]
+    end
     # We also need the duration for which the train stops at MI
-    stopping_duration_at_MI = df[MI_rows[2], :scheduledtime]-df[MI_rows[1], :scheduledtime]
+    if length(MI_rows) == 2
+        stopping_duration_at_MI = df[MI_rows[2], :scheduledtime]-df[MI_rows[1], :scheduledtime]
+    else
+        stopping_duration_at_MI = 0
+    end
     # We also need scheduled time to reach each further block after exiting MI
-    df_after_MI[!, :block_reach_time] .= df_after_MI[!, :scheduledtime] .- df_after_MI[1, :scheduledtime]
+    if nrow(df_after_MI) > 0
+        df_after_MI[!, :block_reach_time] .= df_after_MI[!, :scheduledtime] .- df_after_MI[1, :scheduledtime]
+    end
 
     # Get the schedule along the alternate Pottendorfer Linie
-    departure_time_at_NB = df[NB_rows[2], :scheduledtime]
+    if length(NB_rows) == 2
+        departure_time_at_NB = df[NB_rows[2], :scheduledtime]
+    else
+        departure_time_at_NB = df[NB_rows[1], :scheduledtime]
+    end
     df_pottendorfer = pottendorfer_schedule(departure_time_at_NB)
     df_pottendorfer[!, :trainid] .= trainid
     df_pottendorfer[!, :transittype] .= "p"
-    df_pottendorfer[1, :transittype] = "d"
-    df_pottendorfer[end, :transittype] = "a"
+    if length(NB_rows) == 2
+        df_pottendorfer[1, :transittype] = "d"
+    else
+        df_pottendorfer[1, :transittype] = "b"
+    end
+    if length(MI_rows)== 2
+        df_pottendorfer[end, :transittype] = "a"
+    elseif length(MI_rows)== 1
+        df_pottendorfer[end, :transittype] = "e"
+    end
     df_pottendorfer[!, :direction] .= 2
-    df_pottendorfer[!, :line] .= "10601"
+    df_pottendorfer[!, :line] .= 10601
 
     select!(df_pottendorfer, [:trainid, :bst, :transittype, :direction, 
                               :line, :distance, :scheduledtime])
 
     # Now correct the distance along Pottendorfer by adding distance covered
     # upto NB
-    df_pottendorfer[!, :distance] .+= df_upto_NB[end, :distance]
-    # Correct the scheduledtime along Sudbahn after exiting MI 
-    df_after_MI[!, :scheduledtime] = df_pottendorfer[end, :scheduledtime] .+ df_after_MI[!, :block_reach_time] 
-    # We should correct this scheduledtime further by adding stopping duration at MI
-    df_after_MI[!, :scheduledtime] .+= stopping_duration_at_MI 
-    # Finally, correct the distance along Sudbahn after exiting MI
-    df_after_MI[!, :distance] .= df_after_MI[!, :block_length] .+ df_pottendorfer[end, :distance]
-    select!(df_after_MI, Not([:block_length, :block_reach_time]))
+    if nrow(df_upto_NB) > 0
+        df_pottendorfer[!, :distance] .+= df_upto_NB[end, :distance]
+    end
+    println(first(df_pottendorfer, 5))
+    if nrow(df_after_MI) > 0
+        # Correct the scheduledtime along Sudbahn after exiting MI 
+        df_after_MI[!, :scheduledtime] = df_pottendorfer[end, :scheduledtime] .+ df_after_MI[!, :block_reach_time] 
+        # We should correct this scheduledtime further by adding stopping duration at MI
+        df_after_MI[!, :scheduledtime] .+= stopping_duration_at_MI 
+        # Finally, correct the distance along Sudbahn after exiting MI
+        df_after_MI[!, :distance] .= df_after_MI[!, :block_length] .+ df_pottendorfer[end, :distance]
+        select!(df_after_MI, Not([:block_length, :block_reach_time]))
+    end
     append!(df_upto_NB, df_pottendorfer, cols=:setequal, promote=true)
-    append!(df_upto_NB, df_after_MI, cols=:setequal) # df_upto_NB now contains all the data up to the last station
+    append!(df_upto_NB, df_after_MI, cols=:setequal, promote=true) # df_upto_NB now contains all the data up to the last station
     empty!(df)
     df = df_upto_NB
     df_upto_NB = Nothing
@@ -152,8 +189,7 @@ function reroute_sudbahn_to_pottendorfer!(df_full; trainid="RJ_130")
     trainid_end_ix = findall(df_full[!, :trainid] .== trainid)[end]
     #df_full = vcat(df_full[1:trainid_start_ix-1, :], df, df_full[trainid_end_ix+1:end, :])
     filter!(row -> row[:trainid] .!= trainid, df_full)
-    append!(df_full, df)
+    append!(df_full, df, promote=true)
     sort!(df_full, :trainid)
     #return df_full
 end
-
